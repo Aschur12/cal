d75f1427c654fb6245ffa2a5b51309912236d542
Andrey <aschur1979@gmail.com>
2021-12-03 20:13:13 +0200

    1

--- client/src/components/main.js
+++ client/src/components/main.js
@@ -1,12 +1,34 @@
 import {Component} from "react";
 import NavBar from "./navbar";
+import '../main.css'
 
 class Mainpage extends Component {
     render() {
         return (
-            
-            <NavBar/>
-            
+        <section className="main_page">
+                <div className="container">
+                <NavBar/>,
+                <div className="main_header">
+                    <h1>Инжиниринговая компания "КМА Технология"</h1>
+                    <h2>полный спектр услуг связанных с:<br/>
+                        поставкой и внедрением передового оборудования и технологий<br/>
+                        холодной и гарячей штамповки<br/><br/></h2>
+                    <h2>Официальный дистрибьютор</h2>
+                </div>
+                <div className="main_btn_2">
+                    <button className="main_cold">Холодная штамповка</button>
+                    <button className="main_hot">Горячая штамповка</button>
+                </div>
+                <div className="logo_simpac_jnh">
+                    <img src="./img/SIMPAC_logo.png" alt="Simpac"></img>
+                    <img src="./img/JNH_logo.png" alt="JNH"></img>
+                </div>
+                <div className="contacts">
+                    <div>Email: kmatechnologyua@gmail.com</div>
+                    <div>Тел.: +38(067)538-60-60</div>
+                </div>
+            </div>
+    </section>
         )
     }
 }
--- client/src/components/navbar.js
+++ client/src/components/navbar.js
@@ -4,20 +4,16 @@ import '../main.css'
 class NavBar extends Component {
   render() {
     return (
-      <div className="main_page">
-      <div className="container">
       <div className="navbar">
                 <div className="div_logo">
-                    <img className="logo" src="./img/KMA_Logo.png" alt="KMA Technology"></img>
+                    {/* <img className="logo" src="./img/KMA_Logo.png" alt="KMA Technology"></img> */}
                 </div>
                 <button className="main_btn">Главная</button>
                 <button className="main_btn">Оборудование</button>
                 <button className="main_btn">Контакты</button>
                 <button className="main_btn">Login</button>
-            </div>
-    </div>
-    </div>);
-  }
+        </div>
+    )};
 }
 
 export default NavBar
